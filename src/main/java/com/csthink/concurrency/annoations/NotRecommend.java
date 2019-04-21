91337c3607e5a88d1cb6d9fc185e8cc3a7b484df
杜明洋 <security.2009@live.cn>
2019-04-21 11:37:40 +0800

    修改注解接口所在包

--- src/main/java/com/csthink/concurrency/annoations/NotRecommend.java
+++ src/main/java/com/csthink/concurrency/annoations/NotRecommend.java
@@ -1,4 +1,4 @@
-package com.csthink.concurrency.com.csthink.annoations;
+package com.csthink.concurrency.annoations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,11 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 用来标记 「线程安全」的类或写法
+ * 用来标记 「不推荐」的类或写法
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
-public @interface ThreadSafe {
+public @interface NotRecommend {
 
     String value() default "";
 }
